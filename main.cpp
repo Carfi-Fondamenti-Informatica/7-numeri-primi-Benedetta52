f62d499afea612447ffb8aff57c05daeb60d7970
Benedetta52 <91663828+Benedetta52@users.noreply.github.com>
2021-12-02 07:50:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int numero;
+    cin >> numero;
+    if ( numero == 0){
+        cout << "numero primo" << endl;
+    }else if ( numero < 0){
+        return 0;
+    }else{
 
-int main(){
-  return 0;
+        if ( numeroPrimo (numero) == true){
+            cout << "numero primo" << endl;
+        }else{
+            cout << "numero non primo" << endl;
+        }
+
+    }
+
+    return 0;
 }
